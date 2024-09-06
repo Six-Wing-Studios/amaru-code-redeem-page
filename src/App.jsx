4a3547752a8d283d9de02bda54fe0ff361422d06
Ryan Shiel <93ryans@gmail.com>
2024-09-06 18:22:54 -0400

    added react-toastify for better messaging

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import './App.css'
 import LoginPage from './Screens/LoginPage'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
-  return <LoginPage />
+  return (
+    <>
+      <LoginPage />
+      <ToastContainer 
+        theme='colored'
+        position='top-center'
+      />
+    </>
+  );
 }
 
 export default App
